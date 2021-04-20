2e9aeec4d9e75f63c93d4f0ec9b56465baf86417
Snake5932 <shtjnk@yandex.ru>
2021-04-20 13:41:47 +0300

    reorganised

---
+++ lab2/cmake-build-debug/CMakeFiles/rotating_cube.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rotating_cube.dir/main.cpp.obj"
+  "librotating_cube.dll.a"
+  "rotating_cube.exe"
+  "rotating_cube.exe.manifest"
+  "rotating_cube.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rotating_cube.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
