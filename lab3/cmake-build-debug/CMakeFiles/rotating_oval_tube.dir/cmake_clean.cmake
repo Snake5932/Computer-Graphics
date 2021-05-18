a405d00c2d44202a99cb7a49e2937be85ff37df3
Snake5932 <shtjnk@yandex.ru>
2021-05-18 11:16:51 +0300

    lab3 upd

---
+++ lab3/cmake-build-debug/CMakeFiles/rotating_oval_tube.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rotating_oval_tube.dir/main.cpp.obj"
+  "librotating_oval_tube.dll.a"
+  "rotating_oval_tube.exe"
+  "rotating_oval_tube.exe.manifest"
+  "rotating_oval_tube.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rotating_oval_tube.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
